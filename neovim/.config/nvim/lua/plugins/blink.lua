61287b0f0fad07ae25811d68499dfdba831fbf4b
Dmytro Shcherbakov <vorb@studio.vorbster.guru>
2025-06-06 18:43:00 -0400

    Added copilot and noice

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,5 +1,5 @@
 return {
-    { "L3MON4D3/LuaSnip", keys = {} },
+    { "L3MON4D3/LuaSnip",      keys = {} },
     {
         "saghen/blink.cmp",
         dependencies = {
--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,48 +1,38 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    version = false,
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = 'main'
     },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        sync_install = false,
-	ignode_install = { "javascript " },
-	modules = {},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlightning = false,
-	},
-	indent = { enable = true },
-	auto_install = true,
-	ensure_installed = {
-		"bash",
-		"c",
-		"json",
-		"lua",
-		"luadoc",
-		"luap",
-		"python",
-		"regex",
-		"vim",
-		"vimdoc",
-		"yaml",
-		"rust",
-		"dockerfile",
-		"yaml",
-		"gitignore",
-		"cmake",
-		"cpp",
-		"cuda",
-		"latex",
-		"llvm",
-		"make",
-		"perl",
-	},
-      })
-      end,
-  }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = 'main',
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            require 'nvim-treesitter'.install {
+                "bash",
+                "c",
+                "json",
+                "lua",
+                "luadoc",
+                "luap",
+                "python",
+                "regex",
+                "vim",
+                "vimdoc",
+                "yaml",
+                "rust",
+                "dockerfile",
+                "yaml",
+                "gitignore",
+                "cmake",
+                "cpp",
+                "cuda",
+                "latex",
+                "llvm",
+                "make",
+                "perl",
+            }
+        end,
+    },
 }
