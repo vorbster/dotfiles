34b93e6c57e1795511b90730985656aae2fbbae5
Dmytro Shcherbakov <vorb@mbp.local>
2025-09-16 15:41:07 -0400

    Updated neovim theme to dracula

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,18 @@
-return {
+--[[ return {
     {
       "rebelot/kanagawa.nvim",
       name = "kanagawa",
       config = function()
        vim.cmd ("colorscheme kanagawa")
       end,
+    }
+} ]]
+return {
+    {
+        "Mofiqul/dracula.nvim",
+        name = "dracula",
+        config = function()
+            vim.cmd ("colorscheme dracula")
+        end,
     }
 }
--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
       config = function()
             require('lualine').setup {
               options = {
-                    theme = "kanagawa",
+                    theme = "dracula",
               },
             }
       end
