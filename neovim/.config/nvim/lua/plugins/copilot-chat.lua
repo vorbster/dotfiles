53ea0355e059922e60a3e18deee367274d7c2f44
Dima S. <git@vorbster.guru>
2026-04-12 22:19:05 -0400

    Updated noice config and copilot-chat

--- neovim/.config/nvim/lua/plugins/copilot-chat.lua
+++ neovim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -3,3 +3,14 @@ vim.pack.add ({
     'https://github.com/nvim-lua/plenary.nvim.git',
     'https://github.com/github/copilot.vim.git'
 })
+
+-- Redefine the default mappings for CopilotChat for tab to work
+
+require('CopilotChat').setup({
+  mappings = {
+    complete = {
+      detail = 'Use C-Space for completion',
+      insert = '<C-Space>',
+    },
+  },
+})
