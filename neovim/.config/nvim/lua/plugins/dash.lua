13c822152da020dc6b5728244518368df83cf26d
Dima S. <git@vorbster.guru>
2026-06-20 09:52:53 -0400

    Added a dash

---
+++ neovim/.config/nvim/lua/plugins/dash.lua
@@ -0,0 +1,79 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = function(_, opts)
+      math.randomseed(os.time())
+      local choose = function(choices)
+        return choices[math.random(1, #choices)]
+      end
+      opts.lualine_bold = true
+      opts.on_highlights = function(hl, c)
+        hl["SnacksDashboardHeader"] = {
+          fg = choose({
+            c.blue,
+            c.blue1,
+            c.blue2,
+            c.blue5,
+            c.cyan,
+            c.green,
+            c.green2,
+            c.magenta,
+            c.orange,
+            c.purple,
+            c.red,
+            c.teal,
+            c.yellow,
+          }),
+        }
+        hl["SnacksDashboardTitle"] = { fg = c.magenta, bold = true }
+        hl["SnacksDashboardKey"] = { fg = c.orange, bold = true }
+      end
+      return opts
+    end,
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        width = 40,
+        sections = function()
+          local header = [[
+      ████ ██████           █████      ██                    
+     ███████████             █████                            
+     █████████ ███████████████████ ███   ███████████  
+    █████████  ███    █████████████ █████ ██████████████  
+   █████████ ██████████ █████████ █████ █████ ████ █████  
+ ███████████ ███    ███ █████████ █████ █████ ████ █████ 
+██████  █████████████████████ ████ █████ █████ ████ ██████
+]]
+          local function greeting()
+            local hour = tonumber(vim.fn.strftime("%H"))
+            -- [02:00, 10:00) - morning, [10:00, 18:00) - day, [18:00, 02:00) - evening
+            local part_id = math.floor((hour + 6) / 8) + 1
+            local day_part = ({ "evening", "morning", "afternoon", "evening" })[part_id]
+            local username = os.getenv("USER") or os.getenv("USERNAME") or "user"
+            return ("Good %s, %s"):format(day_part, username)
+          end
+
+          -- stylua: ignore
+          return {
+            { padding = 0, align = "center", text = { header, hl = "header" } },
+            { padding = 2, align = "center", text = { greeting(), hl = "header" } },
+            { title = "Builtin Actions", indent = 2, padding = 1,
+              { icon = " ", key = "f", desc = "Find File",       action = ":lua Snacks.dashboard.pick('files')" },
+              { icon = " ", key = "n", desc = "New File",        action = ":ene | startinsert" },
+              { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+              { icon = " ", key = "q", desc = "Quit",            action = ":qa" } },
+            { title = "Recent Projects", section = "projects", indent = 2, padding = 1 },
+            { title = "Maintenance Actions", indent = 2, padding = 2,
+              { icon = " ", key = "c", desc = "Config",      action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
+              { icon = "󰒲 ", key = "l", desc = "Lazy",        action = ":Lazy" },
+              { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+              { icon = "󱁤 ", key = "m", desc = "Mason",       action = ":Mason" },                          },
+            { section = "startup" },
+          }
+        end,
+      },
+    },
+  },
+}
